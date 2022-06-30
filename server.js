32d64c2b8e9915adc5cde8703f86bc19d2c206a3
David Abell <david.abell.m@gmail.com>
2022-06-30 15:02:38 +0100

    update originWhitelist

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-const corsProxy = require('cors-anywhere');
+const corsProxy = require("cors-anywhere");
 
 // Listen on a specific host via the HOST environment variable.
-const host = process.env.HOST || '0.0.0.0';
+const host = process.env.HOST || "0.0.0.0";
 // Listen on a specific port via the PORT environment variable.
 const port = process.env.PORT || 8080;
 
-corsProxy.createServer({
-    originWhitelist: ['http://localhost:3000', 'http://localhost:3000/dashboard/settings', 'https://portfolio-ravenous.herokuapp.com'],
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, () => {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
+corsProxy
+  .createServer({
+    originWhitelist: [
+      "http://localhost:3000",
+      "http://localhost:3000/dashboard/settings",
+    ],
+    requireHeader: ["origin", "x-requested-with"],
+    removeHeaders: ["cookie", "cookie2"],
+  })
+  .listen(port, host, () => {
+    console.log("Running CORS Anywhere on " + host + ":" + port);
+  });
